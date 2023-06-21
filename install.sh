1dc9fe35a85e3479e60c0277e98fd742595105ea
root <dominik.geissler@rwth-aachen.de>
2023-06-21 22:38:59 +0200

    rename script and added alias in install

--- install.sh
+++ install.sh
@@ -9,5 +9,16 @@ export PATH="${PATH}:/root/bin/gam/gam"
 # Shorthand to executable
 function gam() { "/root/bin/gam/gam" "$@" ; }
 
+# Add alias
+alias gam=/root/bin/gam/gam
+
 # Start the interactive creation
 gam create project
+
+# After completing the GAM setup some things might not work.
+# TODO: in readme
+
+# 1. 403 on configured apps: -> superadmin
+# 2. ERROR: User <...>@studenten-bilden-schueler.de: unauthorized_client: Client is unauthorized to retrieve access tokens using this method, or client not authorized for any of the scopes requested.
+#   -> run 'gam user <username> check serviceaccount' and follow
+#   cf. https://github.com/sbsev/google-workspace/issues/14
