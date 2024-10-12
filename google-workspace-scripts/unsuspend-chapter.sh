2b4e5e28b8bdb5e18cd7627a1790bea3789d4a94
dominikgeissler <dominik.geissler@rwth-aachen.de>
2024-10-12 12:48:13 +0200

    Fixed syntax error in suspend-chapter, created unsuspend-chapter and changed to correct mail prefixes

---
+++ google-workspace-scripts/unsuspend-chapter.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# This script requires authorized gam scope
+
+# Path to GAM executable
+# Should be this of you installed it correctly
+GAM_EXC_PATH="/root/bin/gam/gam"
+
+echo -n "Please enter a city name: "
+read cityName
+
+city=$(echo $cityName | tr "[:upper:]" "[:lower:]")
+
+# Set the mapping
+# emailprefix <-> name in google workspace
+declare -A divisions=([schueler]=Schüler [studierende]=Studierende [info]=Kommunikation)
+
+for div in "${!divisions[@]}";
+do
+  echo "$div"
+  $GAM_EXC_PATH update user $div.$city suspended off
+done
